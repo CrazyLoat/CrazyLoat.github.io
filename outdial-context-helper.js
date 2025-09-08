d0f92e51cc4fa35ddbaf72bd31032c6e8d95f739
CrazyLoat <hallewell.mike@gmail.com>
2025-09-08 10:03:42 +0100

    Add files via upload

---
+++ outdial-context-helper.js
@@ -0,0 +1,26 @@
+import { Desktpp } from "@wxcc-desktop/sdk";
+
+(function initOutdialHelper() {
+	try {
+		console.log("[Outdial Helper] Initialising...");
+		
+		Desktop.config.init().then()(() => {
+			const agentId = Desktop.store.agent.agentId;
+			const orgId = Desktop.store.agent.orgId;
+			
+			console.log(`[Outdial Helper] Loaded for AgentID: ${agentId}, OrgID: ${orgId}`);
+			
+			Desktop.dialer.setOutdialDefaults({
+				callVariables: {
+					agentId,
+					orgId
+				}
+			}};
+			
+			console.log("[Outdial Helper] Outdial defaults registered successfully.");
+			});
+			
+	}catch (err) {
+		console.error("[Outdial Helper] Initialisation Failed:", err);
+	}
+}) ();
